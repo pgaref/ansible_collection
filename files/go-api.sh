a1438f0a78424c77b3f90fe2875bdbac97e17ae6
pgaref <pgaref@macbook-air.lan>
2015-08-21 11:23:51 +0100

    Olly's extra env vars

--- files/go-api.sh
+++ files/go-api.sh
@@ -1,1 +1,4 @@
 export API="odds_parser:564\$TERRG\$ERF\$"
+export MANDRILL="6oUG1HIg5Fq589cvH9ASUQ"
+export STRIPE_SECRET_KEY="sk_live_M1fRP1EhCIeBxsfRj6IkBdYL"
+export STRATABET_URL="https://www.stratabet.com/"
